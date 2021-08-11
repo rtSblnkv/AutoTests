27386e770a8b3857526f757d83f42e0aac08f890
rtSblnkv <artsab001@gmail.com>
2021-08-11 14:19:45 +0400

    pojo classes added

---
+++ src/main/java/EndPoints.java
@@ -0,0 +1,11 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+public final class EndPoints {
+    public static final String USERS_BASE_PATh = "/users/{id}";
+    public static final String RESOURCES_BASE_PATH = "/unknown/{id}";
+
+
+}
--- src/test/java/RestTest.java
+++ src/test/java/RestTest.java
@@ -1,16 +1,24 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 
 public class RestTest{
+
+    public static final RequestSpecification REQ_SPEC = new RequestSpecBuilder()
+            .setBaseUri("https://reqres.in")
+            .setBasePath("/unknown")
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
     @Test
     public void getUsers(){
-        String res =  given().
-                baseUri("https://reqres.in/api")
-                .basePath("/unknown")
-                .contentType(ContentType.JSON)
+        String res =  given().spec(REQ_SPEC)
                 .when().get()
                 .then().statusCode(200)
                 .extract().asString();
