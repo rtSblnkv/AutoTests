142d2d5d2870d5a75185c9c5dde5a30f72314cad
rtSblnkv <artsab001@gmail.com>
2021-08-16 13:00:44 +0400

    resources tests created

--- src/test/java/api/ResourceApiTest.java
+++ src/test/java/api/ResourceApiTest.java
@@ -1,14 +1,42 @@
 package api;
 
+import models.Resource;
+import models.User;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ResourceApiTest {
 
+    private static ResourceApi api;
+
+    @BeforeAll
+    public static void initializeApi()
+    {
+        api = new ResourceApi();
+    }
     @Test
     void getResources() {
+        List<Resource> resources = api.getResources();
+
+        System.out.println(resources);
+
+        assertThat(resources)
+                .isNotNull()
+                .extracting(Resource::getName).contains("cerulean");
     }
 
     @Test
     void getResource() {
+
+        Resource resource = api.getResource(1);
+        System.out.println(resource);
+
+        assertThat(resource)
+                .isNotNull()
+                .extracting(Resource::getName).isEqualTo("cerulean");
     }
 }
